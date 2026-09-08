d5a7ab6d9e9da06637a5114d8c8b21087bb14496
septalfauzan <alfauzansepta@gmail.com>
2026-09-08 17:54:39 +0700

    fix: pin swagger-ui CDN to exact 5.32.14 version

--- app/server/handlers/docs.go
+++ app/server/handlers/docs.go
@@ -12,14 +12,14 @@ const swaggerUIHTML = `<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Saku API Docs</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.32.14/swagger-ui.css">
   <style>
     body { margin: 0; }
   </style>
 </head>
 <body>
   <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.32.14/swagger-ui-bundle.js"></script>
   <script>
     window.onload = function () {
       window.ui = SwaggerUIBundle({
